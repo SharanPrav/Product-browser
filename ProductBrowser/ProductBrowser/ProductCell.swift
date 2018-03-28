50b9ea2516b7fa7f372a269dd602e9d73b60a20f
Saranya Ravi <sharan729@gmail.com>
2018-03-28 23:28:07 +0200

    cadview like display for tableview of items

--- ProductBrowser/ProductBrowser/ProductCell.swift
+++ ProductBrowser/ProductBrowser/ProductCell.swift
@@ -12,10 +12,15 @@ class ProductCell: UITableViewCell {
 
     @IBOutlet weak var thumbnailImage: UIImageView!
     @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productCardBackground: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        thumbnailImage.contentMode = .scaleAspectFit
+        thumbnailImage.contentMode = .scaleAspectFit;
+        
+        productCardBackground.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+        productCardBackground.layer.shadowOffset = CGSize(width:0, height:0)
+        productCardBackground.layer.shadowOpacity = 0.8
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
